683bbcf6a8c6b85fe18be7bad69ec1b1ba3bb0e6
Pascal Naviere <pascal.naviere@adevinta.com>
2021-03-28 03:53:47 +0200

    (tools): add uiSwagger

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { ErrorValidateModel } from 'src/error-midleware-jsend'
 import { SwaggerSpecChecker } from 'src/core'
+import swaggerUi from 'swagger-ui-express'
 
 export default {
     SwaggerSpecChecker,
-    ErrorValidateModel
+    ErrorValidateModel,
+    swaggerUi
 }
